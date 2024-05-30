ce42b7856b075ed981ca864fd403f2290011f3d6
RoshanDalami <roshandalami0@gmail.com>
2024-05-30 15:47:40 +0545

    handle cookies

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -1,7 +1,7 @@
 import { Admin } from "../Model/admin.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import bcryptjs from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 export async function CreateAdmin(req, res) {
   try {
     const { email, password, username, contactNumber } = req.body;
@@ -9,7 +9,11 @@ export async function CreateAdmin(req, res) {
     if (!email || !password || !username || !contactNumber) {
       return res
         .status(400)
-        .json(new ApiResponse(new ApiResponse(400, null, "All fields are required!!!")));
+        .json(
+          new ApiResponse(
+            new ApiResponse(400, null, "All fields are required!!!")
+          )
+        );
     }
     const user = await Admin.findOne({ email: email });
     if (user)
@@ -17,15 +21,17 @@ export async function CreateAdmin(req, res) {
         .status(400)
         .json(new ApiResponse(400, null, "User already exist with this email"));
 
-    const hashedPassword = await bcryptjs.hash(password,10)
+    const hashedPassword = await bcryptjs.hash(password, 10);
     const newUser = new Admin({
       email,
-      password:hashedPassword,
+      password: hashedPassword,
       username,
       contactNumber,
     });
     const savedUser = await newUser.save();
-    return res.status(201).json(new ApiResponse(200,null, "user created successfully"));
+    return res
+      .status(201)
+      .json(new ApiResponse(200, null, "user created successfully"));
   } catch (error) {
     console.log(error);
     return res
@@ -37,25 +43,50 @@ export async function CreateAdmin(req, res) {
 export async function LoginAdmin(req, res) {
   try {
     const { email, password } = req.body;
-    const user = await Admin.findOne({email:email});
-    console.log(user)
-    if(!user) return res.status(400).json(new ApiResponse(400,null,"User doesn't exist with this email"));
+    const user = await Admin.findOne({ email: email });
+    console.log(user);
+    if (!user)
+      return res
+        .status(400)
+        .json(new ApiResponse(400, null, "User doesn't exist with this email"));
 
-    const isPasswordMatched = await bcryptjs.compare(password,user.password);
+    const isPasswordMatched = await bcryptjs.compare(password, user.password);
 
-    if(!isPasswordMatched) return res.status(400).json(new ApiResponse(400,null,"Invalid password!!!"));
-    
-    const userWithoutPassword = await Admin.findOne({email:email}).select('-password');
+    if (!isPasswordMatched)
+      return res
+        .status(400)
+        .json(new ApiResponse(400, null, "Invalid password!!!"));
 
-    const token = await jwt.sign({
-        user:{id:user._id},
+    const userWithoutPassword = await Admin.findOne({ email: email }).select(
+      "-password"
+    );
 
-    },process.env.TOKEN_SECRET,{
-        expiresIn:'1d'
+    const token = await jwt.sign(
+      {
+        user: { id: user._id },
+      },
+      process.env.TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Lax", // Adjust as necessary: 'Strict', 'Lax', or 'None'
+      path: "/", // Ensure the cookie is accessible from the entire site
     });
 
-    return res.status(200).json(new ApiResponse(200,{accessToken:token,userDetail:userWithoutPassword},"Login success"))
-
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken: token, userDetail: userWithoutPassword },
+          "Login success"
+        )
+      );
   } catch (error) {
     return res
       .status(500)
@@ -63,15 +94,17 @@ export async function LoginAdmin(req, res) {
   }
 }
 
-export async function admin(req,res){
-    try {
-        const userId = req.user.id
-        console.log(userId)
-        const response = await Admin.findOne({_id:userId}).select("-password");
-        return res.status(200).json(new ApiResponse(200,response,"User information generated"))
-
-    } catch (error) {
-        return res.status(500).json(new ApiResponse(500,null,"Internal Server Error"))
-    }
+export async function admin(req, res) {
+  try {
+    const userId = req.user.id;
+    console.log(userId);
+    const response = await Admin.findOne({ _id: userId }).select("-password");
+    return res
+      .status(200)
+      .json(new ApiResponse(200, response, "User information generated"));
+  } catch (error) {
+    return res
+      .status(500)
+      .json(new ApiResponse(500, null, "Internal Server Error"));
+  }
 }
-
--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,7 @@
 import { User } from "../Model/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import bcryptjs from 'bcryptjs'
-import jwt from 'jsonwebtoken'
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 export async function CreateUser(req, res) {
   try {
     const { email, password, username, contactNumber } = req.body;
@@ -9,7 +9,11 @@ export async function CreateUser(req, res) {
     if (!email || !password || !username || !contactNumber) {
       return res
         .status(400)
-        .json(new ApiResponse(new ApiResponse(400, null, "All fields are required!!!")));
+        .json(
+          new ApiResponse(
+            new ApiResponse(400, null, "All fields are required!!!")
+          )
+        );
     }
     const user = await User.findOne({ email: email });
     if (user)
@@ -17,15 +21,17 @@ export async function CreateUser(req, res) {
         .status(400)
         .json(new ApiResponse(400, null, "User already exist with this email"));
 
-    const hashedPassword = await bcryptjs.hash(password,10)
+    const hashedPassword = await bcryptjs.hash(password, 10);
     const newUser = new User({
       email,
-      password:hashedPassword,
+      password: hashedPassword,
       username,
       contactNumber,
     });
     const savedUser = await newUser.save();
-    return res.status(201).json(new ApiResponse(200,null, "user created successfully"));
+    return res
+      .status(201)
+      .json(new ApiResponse(200, null, "user created successfully"));
   } catch (error) {
     console.log(error);
     return res
@@ -37,27 +43,48 @@ export async function CreateUser(req, res) {
 export async function LoginUser(req, res) {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({email:email});
-    console.log(user)
-    if(!user) return res.status(400).json(new ApiResponse(400,null,"User doesn't exist with this email"));
+    const user = await User.findOne({ email: email });
+    console.log(user);
+    if (!user)
+      return res
+        .status(400)
+        .json(new ApiResponse(400, null, "User doesn't exist with this email"));
 
-    const isPasswordMatched = await bcryptjs.compare(password,user.password);
+    const isPasswordMatched = await bcryptjs.compare(password, user.password);
 
-    if(!isPasswordMatched) return res.status(400).json(new ApiResponse(400,null,"Invalid password!!!"));
-    
-    const userWithoutPassword = await User.findOne({email:email}).select('-password');
+    if (!isPasswordMatched)
+      return res
+        .status(400)
+        .json(new ApiResponse(400, null, "Invalid password!!!"));
 
-    const token = await jwt.sign({
-        user:{id:user._id},
+    const userWithoutPassword = await User.findOne({ email: email }).select(
+      "-password"
+    );
 
-    },process.env.TOKEN_SECRET,{
-        expiresIn:'1d'
+    const token = await jwt.sign(
+      {
+        user: { id: user._id },
+      },
+      process.env.TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "Lax", // Adjust as necessary: 'Strict', 'Lax', or 'None'
+      path: "/", // Ensure the cookie is accessible from the entire site
     });
-
-    return res.status(200).json(new ApiResponse(200,{accessToken:token,userDetail:userWithoutPassword},"Login success"))
-
-    return res.status(200).json(new ApiResponse(200,user,"Login success"))
-
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken: token, userDetail: userWithoutPassword },
+          "Login success"
+        )
+      );
   } catch (error) {
     return res
       .status(500)
@@ -65,15 +92,17 @@ export async function LoginUser(req, res) {
   }
 }
 
-export async function Me(req,res){
-    try {
-        const userId = req.user.id
-        
-        const response = await User.findOne({_id:userId}).select("-password");
-        return res.status(200).json(new ApiResponse(200,response,"User information generated"))
+export async function Me(req, res) {
+  try {
+    const userId = req.user.id;
 
-    } catch (error) {
-        return res.status(500).json(new ApiResponse(500,null,"Internal Server Error"))
-    }
+    const response = await User.findOne({ _id: userId }).select("-password");
+    return res
+      .status(200)
+      .json(new ApiResponse(200, response, "User information generated"));
+  } catch (error) {
+    return res
+      .status(500)
+      .json(new ApiResponse(500, null, "Internal Server Error"));
+  }
 }
-
